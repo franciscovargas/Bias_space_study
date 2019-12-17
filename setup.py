721523e01d09897847c12cd4177d1cecbfe1328a
franciscovargas <vargfran@gmail.com>
2019-12-17 21:03:20 +0000

    Setup.py to package

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+from pip.req import parse_requirements
+
+here = path.abspath(path.dirname(__file__))
+install_reqs = parse_requirements(here + '/requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='L101_Bias_space_study',
+    version='1.0',
+    description='L101 project on Bias in word embeddings',
+    long_description=long_description,
+    url='https://github.com/franciscovargas/L101_Bias_space_study.git',
+    author='Francisco Vargas',
+    author_email='vargfran@gmail.com',
+    license='MIT',
+    keywords='NLP, Kernel Methods, RKHS, Linear Algebra, PCA, Kernel PCA',
+    install_requires=reqs,
+)
--- utils/mock_model.py
+++ utils/mock_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 from collections import OrderedDict
 import gensim
+import warnings
 
 
 class MockModel(object):
@@ -10,13 +11,14 @@ class MockModel(object):
         name, extension = file_name.split(".")
         bin = True if "bin" in extension else False
 
-        mod = gensim.models.Word2Vec.load_word2vec_format(file_name, binary=bin)#
+        mod = gensim.models.Word2Vec.load_word2vec_format(file_name, binary=bin)
 
         return cls(mod.wv.index2word, mod.wv.vectors)
 
     def __init__(self, word_list, X):
         self.key  = OrderedDict(index2word, zip(list(range(len(word_list)))))
         self.X = X
+        self.n, self.dim = X.shape
         self.index2word = index2word
 
     def __call__(self, w):
@@ -30,4 +32,5 @@ class MockModel(object):
         elif w.lower().strip() in self.key:
             return self.X[self.key[w.lower().strip()], :]
         else:
-            raise BaseException(f"Word {w} not in model")
+            warnings.warn(f"Word {w} not in model", RuntimeWarning)
+            return np.zeros(dim)
