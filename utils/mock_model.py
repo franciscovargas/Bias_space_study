d7af51f183a1fffb40a699f06b9cc06974dfd539
franciscovargas <vargfran@gmail.com>
2019-12-17 20:49:01 +0000

    added moc model for WEAT tests

---
+++ utils/mock_model.py
@@ -0,0 +1,33 @@
+import numpy as np
+from collections import OrderedDict
+import gensim
+
+
+class MockModel(object):
+
+    @classmethod
+    def from_file(cls, file_name):
+        name, extension = file_name.split(".")
+        bin = True if "bin" in extension else False
+
+        mod = gensim.models.Word2Vec.load_word2vec_format(file_name, binary=bin)#
+
+        return cls(mod.wv.index2word, mod.wv.vectors)
+
+    def __init__(self, word_list, X):
+        self.key  = OrderedDict(index2word, zip(list(range(len(word_list)))))
+        self.X = X
+        self.index2word = index2word
+
+    def __call__(self, w):
+        return self.get_word_vector(w)
+
+    def get_word_vector(self, w):
+        if w in self.key:
+            return self.X[self.key[w], :]
+        elif w.lower() in in self.key:
+            return self.X[self.key[w.lower()], :]
+        elif w.lower().strip() in self.key:
+            return self.X[self.key[w.lower().strip()], :]
+        else:
+            raise BaseException(f"Word {w} not in model")
